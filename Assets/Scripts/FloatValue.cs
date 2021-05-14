7b8739b7b49945d863a17309e3b1522ea907d05e
Domara Shlimon <domara2000@gmail.com>
2021-05-14 18:28:04 -0400

    added int and float value asset types

---
+++ Assets/Scripts/FloatValue.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Value", menuName = "Scriptable Objects/FloatValue")]
+public class FloatValue : ScriptableObject {
+	public float Value = 0.0f;
+}
---
+++ Assets/Scripts/IntValue.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Value", menuName = "Scriptable Objects/IntValue")]
+public class IntValue : ScriptableObject {
+	public int Value = 0;
+}
